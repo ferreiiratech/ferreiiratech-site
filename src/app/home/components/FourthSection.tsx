deb7c7d79adbd671292ff27b0b14af096797738a
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-04-03 14:19:59 -0300

    refactor: atualiza as cores dos títulos de branco para cinza

--- src/app/home/components/FourthSection.tsx
+++ src/app/home/components/FourthSection.tsx
@@ -6,7 +6,7 @@ export default function FourthSection() {
     return (
       <section className="bg-secondary text-primary mt-[-2rem] 2xl:mt-[-10rem]">
         <div className="mx-5 xs:mx-10 sm:mx-14 md:mx-24 lg:mx-44 2xl:mx-72">
-          <h1 className="text-center font-spaceGrotesk font-medium text-primary mb-16 text-2xl sm:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
+          <h1 className="text-center font-spaceGrotesk font-medium text-gray-300 mb-16 text-2xl sm:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
             Conheça os <span className="text-highlight font-bold">meus projetos</span>
           </h1>
   
