3120fe2bde94aa94ecf33e5dd63fb7bfa84f16a1
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-08-25 09:26:23 -0300

    desabilita rule noArrayIndexKey do biome

--- src/app/home/components/InfiniteScrollBanner.tsx
+++ src/app/home/components/InfiniteScrollBanner.tsx
@@ -12,22 +12,20 @@ export default function InfiniteScrollBanner() {
   return (
     <div className="relative w-full overflow-hidden bg-black-100 font-spaceGrotesk font-normal text-primary py-3 text-[2rem] md:text-[2.5rem] md:py-6 lg:py-10 lg:text-[3rem]">
       <div className="animate-scroll-left flex whitespace-nowrap">
-        {Array(duplicated)
-          .fill(null)
-          .map((element, _) => (
-            <div key={element} className="flex items-center lg:ml-10">
-              <span>Backend</span>
-              <span className="mx-4 lg:mx-10 text-highlight">-</span>
-              <span>Programação</span>
-              <span className="mx-4 lg:mx-10 text-highlight">-</span>
-              <span>Frontend</span>
-              <span className="mx-4 lg:mx-10 text-highlight">-</span>
-              <span>Tecnologia</span>
-              <span className="mx-4 lg:mx-10 text-highlight">-</span>
-              <span>Devops</span>
-              <span className="mx-4 lg:ml-10 text-highlight">-</span>
-            </div>
-          ))}
+        {Array.from({ length: duplicated }).map((_, index) => (
+          <div key={`banner-${index}`} className="flex items-center lg:ml-10">
+            <span>Backend</span>
+            <span className="mx-4 lg:mx-10 text-highlight">-</span>
+            <span>Programação</span>
+            <span className="mx-4 lg:mx-10 text-highlight">-</span>
+            <span>Frontend</span>
+            <span className="mx-4 lg:mx-10 text-highlight">-</span>
+            <span>Tecnologia</span>
+            <span className="mx-4 lg:mx-10 text-highlight">-</span>
+            <span>Devops</span>
+            <span className="mx-4 lg:ml-10 text-highlight">-</span>
+          </div>
+        ))}
       </div>
     </div>
   )
