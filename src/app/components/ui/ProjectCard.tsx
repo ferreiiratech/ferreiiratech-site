81f3c78cfe8b8ec86d21ef16ada5ad2de9f15885
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-08-25 11:04:23 -0300

    refatora ProjectCard fazendo uso do Card e adicionando campo de status

--- src/app/components/ui/ProjectCard.tsx
+++ src/app/components/ui/ProjectCard.tsx
@@ -1,21 +1,64 @@
+"use client"
+
 import { ExternalLink } from "lucide-react"
 import Link from "next/link"
 import React from "react"
+import Card from "./Card"
+import Image from "next/image"
 
 const ProjectCard = React.memo((props: ProjectCardProps) => {
   return (
-    <div
-      key={props.id}
-      className="border border-[#899099]/20 rounded-lg p-8 flex flex-col h-full hover:border-[#899099]/40 transition-colors duration-300 ease-in-out"
-    >
-      <h2 className="text-xl font-spaceGrotesk font-medium text-primary mb-4">
-        {props.title}
-      </h2>
-      <p className="mb-6 flex-grow text-sm">
-        {props.description.length > 100
-          ? `${props.description.slice(0, 300)}...`
-          : props.description}
-      </p>
+    <Card>
+      <div className="mb-4">
+        <span
+          className={`text-[0.6rem] font-spaceGrotesk px-2 py-1 bg-secondary border border-[#899099]/30 rounded-lg cursor-default border-hover-highlight
+            ${
+              props.status === "Finalizado"
+                ? "text-green-400"
+                : props.status === "Em progresso"
+                  ? "text-yellow-400"
+                  : "text-gray-400"
+            }
+          `}
+        >
+          {props.status}
+        </span>
+      </div>
+
+      <div className="flex flex-row gap-4">
+
+        <div className="w-4/5">
+          <h2 className="text-xl font-spaceGrotesk font-medium text-primary mb-4">
+            {props.title.length > 30
+              ? `${props.title.slice(0, 30)}...`
+              : props.title}
+          </h2>
+
+          <p className="mb-6 flex-grow text-sm">
+            {props.description.length > 100
+              ? `${props.description.slice(0, 300)}...`
+              : props.description}
+          </p>
+        </div>
+
+        <div>
+          {props.images ? (
+            <div className="flex flex-col gap-2">
+              {props.images.slice(0, 2).map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  alt={`Imagem do projeto ${props.title} - ${index + 1}`}
+                  className="object-contain pointer-events-none select-none rounded-lg"
+                  width={120}
+                  height={100}
+                  loading="lazy"
+                />
+              ))}
+            </div>
+          ) : null}
+        </div>
+      </div>
 
       <div className="mt-auto">
         <div className="flex gap-2 mb-6">
@@ -28,6 +71,7 @@ const ProjectCard = React.memo((props: ProjectCardProps) => {
             </span>
           ))}
         </div>
+
         <Link
           href={props.repository}
           className="text-highlight hover:underline text-sm transition-colors duration-300"
@@ -36,7 +80,7 @@ const ProjectCard = React.memo((props: ProjectCardProps) => {
           <ExternalLink className="inline-block ml-1 mb-1" size={14} />
         </Link>
       </div>
-    </div>
+    </Card>
   )
 })
 
