a569d11a9a9419a0fc4194d7cdb1842e15f35c91
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-12-06 04:17:04 -0300

    fix: update image hostname in next.config.ts to raw.githubusercontent.com

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i0.wp.com",
+        hostname: "raw.githubusercontent.com",
       },
     ],
   },
