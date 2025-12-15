7f0d286efa5b925e3530cc000b77ee01aeb789ce
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-12-15 03:10:15 -0300

    feat: cria página inicial da landing page de vendas

---
+++ src/app/site/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/app/components/Header"
+import FirstSection from "@/app/site/components/FirstSection"
+
+export default function SitePage() {
+  return (
+    <>
+      <Header />
+      <FirstSection />
+    </>
+  )
+}
