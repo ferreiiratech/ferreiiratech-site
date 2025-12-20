447c50d4dc6ea01b290d54c1239e5a3bd4486a96
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-12-20 02:59:47 -0300

    refactor: move função getGreeting para utilitário reutilizável

--- src/app/site/components/FirstSection.tsx
+++ src/app/site/components/FirstSection.tsx
@@ -3,19 +3,9 @@ import Button from "@/app/components/ui/button"
 import Description from "@/app/components/ui/description"
 import TitleH1 from "@/app/components/ui/title-h1"
 import { serverEnv } from "@/app/env/server"
+import { getGreeting } from "@/lib/site-utils"
 import Image from "next/image"
 import { features } from "./data"
-
-const getGreeting = (): string => {
-  const currentHour = new Date().getHours()
-  if (currentHour > 5 && currentHour <= 12) {
-    return "bom dia"
-  }
-  if (currentHour > 12 && currentHour <= 18) {
-    return "boa tarde"
-  }
-  return "boa noite"
-}
 
 const message = `Olá, ${getGreeting()}! Gostaria de solicitar um orçamento para criação do meu site.`
 const phoneNumber = serverEnv.PHONE_NUMBER
